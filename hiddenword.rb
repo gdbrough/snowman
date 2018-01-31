70ca56e9d0c7eb51600afd8e502ffd8c327114b1
Ed Ostler <edward.o@simul8.com>
2018-01-31 11:29:24 +0000

    adds test to create array

--- hiddenword.rb
+++ hiddenword.rb
@@ -6,4 +6,12 @@ class HiddenWord
     @decrypted_word = decrypted_word
   end
 
+  def get_word_length
+    return @decrypted_word.length()
+  end
+
+  def encrypt_word()
+    length_of_word = decrypted_word.length
+  end
+
 end
--- specs/hiddenword_spec.rb
+++ specs/hiddenword_spec.rb
@@ -13,9 +13,17 @@ class HiddenWordTest < MiniTest::Test
     assert_equal("snowman", @hiddenword.decrypted_word())
   end
 
-  def test_word_gets_encrypted
-    @hiddenword.encrypt_word()
-    assert_equal("*******", @hiddenword.encrypt_word())
+  def test_length_of_word_is_returned
+    assert_equal(7,@hiddenword.get_word_length())
   end
+
+  def test_write_decrypted_word_to_array
+    assert_equal(["s","n","o","w","m","a","n"], @hiddenword.create_decrypted_array())
+  end
+
+  # def test_word_gets_encrypted
+  #   @hiddenword.encrypt_word()
+  #   assert_equal("*******", @hiddenword.encrypt_word())
+  # end
 
 end
